1a84dbdb95f04031858b7a06f242e0f492a6842d
shubham <shubhamsahare106@.com>
2022-05-24 20:43:00 -0700

    first commit

---
+++ secondpattern/src/secondpattern/Secondpattern.java
@@ -0,0 +1,19 @@
+package secondpattern;
+
+public class Secondpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubint
+		int i,j;
+		for(i=1;i<=5;i++) 
+		{
+			for(j=1;j<=5;j++) {
+				System.out.print(i+" ");
+				
+			}
+			System.out.println();
+		}
+                  
+	}
+
+}
